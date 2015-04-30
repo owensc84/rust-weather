dde5d9864ec233dc42cf82d5ce857a5b2a0d8b73
Chris <owens322@umn.edu>
2015-04-29 20:37:51 -0500

    update toml

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
+#![feature(plugin)]
+#![plugin(regex_macros)]
+
+extern crate hyper;
+extern crate regex;
+
+use regex::Regex;
+
+use std::io::Read;
+
+use hyper::Client;
+use hyper::header::Connection;
+use hyper::header::ConnectionOption;
+
+
+fn determine_zip_code() -> String {
+	let url = format!("http://ip-api.com/json");
+	let mut client = Client::new();
+	let mut res = client.get(url).header(Connection(vec![ConnectionOption::Close])).send().unwrap();
+	let re = Regex::new(r"(?<=zip":")\d{5}").unwrap();
+}
+
+/*fn url_builder(city : String) -> String {
+	let wunder_key = format!("3d58504465810da1");
+	
+} */
 
 fn main() {
+
+	let mut client = Client::new();
 	
-	let str = format!("soon to start");
-	let c : Vec<char> = str.chars().collect();
-	println!("{:?}", c);
+	let mut res = client.get("http://api.wunderground.com/api/3d58504465810da1/features/settings/q/.json")
+        // set a header
+        .header(Connection(vec![ConnectionOption::Close]))
+        // let 'er go!
+        .send().unwrap();
+		
+		let mut body = String::new();
+		res.read_to_string(&mut body).unwrap();
+		
+		println!("Response: {}", body);
 }
